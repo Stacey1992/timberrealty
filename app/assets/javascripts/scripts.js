13453a49d1ee98fdc0a956b35093f2b2f3ffb365
Stacey1992 <stacey.hronowski@gmail.com>
2018-11-21 14:04:09 -0500

    added logo and house pictures

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -41,13 +41,13 @@ $(function() {
         if(bodyScroll > 100){
 
             navbar.addClass("nav-scroll");
-            logo.attr('src', 'img/logo-dark.png');
+            logo.attr('src', 'img/logo-dark.jpg');
 
 
         }else{
 
             navbar.removeClass("nav-scroll");
-            logo.attr('src', 'img/logo-light.png');
+            logo.attr('src', 'img/logo-light2.jpg');
         }
     });
 
--- app/controllers/waitlists_controller.rb
+++ app/controllers/waitlists_controller.rb
@@ -7,11 +7,11 @@ class WaitlistsController < ApplicationController
 	def create
 		@waitlist = Waitlist.new(waitlist_params)
 		if @waitlist.save
-			redirect_to url_for(:controller => "sessions", :action => "landing")
+			redirect_to url_for(:controller => "sessions", :action => "landing2")
 			flash[:notice] = "Thanks! We'll be in touch soon about your free trial!"
 		else
 			flash[:alert] = "Oops! Something went wrong, please try again"
-			redirect_to  controller: "sessions", action: "landing"
+			redirect_to  url_for(controller: "sessions", action: "landing2")
 		end
 	end
 
